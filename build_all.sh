e1caf972c82e090e3fdd7e9f1c28737aa615c1b3
mike seiler <snissn@gmail.com>
2022-09-28 15:24:59 -1000

    support flexible command in build all

--- build_all.sh
+++ build_all.sh
@@ -1,5 +1,14 @@
+
+
+#set cmd to be swagger-codegen if that doesnt exist make it the java command
+#swagger-codegen is supported by brew install swagger-codegen
+#the java command is supported by the docker image swaggerapi/swagger-codegen-cli
+
+cmd="swagger-codegen"
+$cmd || cmd="java -jar /opt/swagger-codegen-cli/swagger-codegen-cli.jar generate"
+
 for lang in $(cat supported_langs); do
-  swagger-codegen generate \
+  $cmd generate \
     -i https://raw.githubusercontent.com/application-research/estuary/master/docs/swagger.json \
     -l $lang \
     -o $lang
