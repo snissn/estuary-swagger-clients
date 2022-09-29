cb2ed66bfd23dc2463ea177c379b338b3ca4e3b1
mike seiler <snissn@gmail.com>
2022-09-28 22:29:40 -1000

    only run if swagger.json on prod s updated

--- build_all.sh
+++ build_all.sh
@@ -7,11 +7,17 @@
 cmd="swagger-codegen"
 $cmd || cmd="java -jar /opt/swagger-codegen-cli/swagger-codegen-cli.jar"
 
-for lang in $(cat supported_langs); do
-  rm -rf $lang
-  $cmd generate \
-    -i https://raw.githubusercontent.com/application-research/estuary/master/docs/swagger.json \
-    -l $lang \
-    -o $lang
 
-done
+swagger_url=https://raw.githubusercontent.com/application-research/estuary/master/docs/swagger.json
+#download swagger.json from github and exit early if its identical to swagger.json in the repo
+curl $swagger_url -o swagger.json
+
+git status swagger.json | grep modified && {
+  for lang in $(cat supported_langs); do
+    rm -rf $lang
+    $cmd generate \
+      -i https://raw.githubusercontent.com/application-research/estuary/master/docs/swagger.json \
+      -l $lang \
+      -o $lang
+  done
+}
