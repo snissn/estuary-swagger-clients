b96940609c2e78cf073f7172be019252d661e78a
mike seiler <snissn@gmail.com>
2022-09-28 22:45:52 -1000

    check in change here to make testing easier but need to change later

--- build_all.sh
+++ build_all.sh
@@ -1,4 +1,7 @@
 
+#TODO change
+repo_owner=application-research
+repo_owner=snissn
 
 #set cmd to be swagger-codegen if that doesnt exist make it the java command
 #swagger-codegen is supported by brew install swagger-codegen
@@ -8,7 +11,7 @@ cmd="swagger-codegen"
 $cmd || cmd="java -jar /opt/swagger-codegen-cli/swagger-codegen-cli.jar"
 
 
-swagger_url=https://raw.githubusercontent.com/application-research/estuary/master/docs/swagger.json
+swagger_url=https://raw.githubusercontent.com/$repo_owner/estuary/master/docs/swagger.json
 #download swagger.json from github and exit early if its identical to swagger.json in the repo
 curl $swagger_url -o swagger.json
 
@@ -16,7 +19,7 @@ git status swagger.json | grep modified && {
   for lang in $(cat supported_langs); do
     rm -rf $lang
     $cmd generate \
-      -i https://raw.githubusercontent.com/application-research/estuary/master/docs/swagger.json \
+      -i $swagger_url \
       -l $lang \
       -o $lang
   done
